364e8f63e4401853efc4edf13961a59bc72cc9fa
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-06-03 18:31:33 +0100

    Injecting Custom Properties
    Demo

--- src/main/java/com/sprintboot/demo/MySprintBootApp/rest/FunRestController.java
+++ src/main/java/com/sprintboot/demo/MySprintBootApp/rest/FunRestController.java
@@ -2,12 +2,27 @@ package com.sprintboot.demo.MySprintBootApp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+	//INJECT PROPERTIES
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	
+	//EXPOSE "/" MAPPING
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + " Team: " + teamName;
+	}
+	
 	//EXPOSE "/" MAPPING
 	@GetMapping("/")
 	public String sayHello() {
@@ -21,7 +36,7 @@ public class FunRestController {
 	}
 	
 	// ENDPOINT MAPPING - GETLUCKY
-	@GetMapping("/getluck")
+	@GetMapping("/fortune")
 	public String getLuckyDay() {
 		return "Today is for lucky day!!!";
 	}
